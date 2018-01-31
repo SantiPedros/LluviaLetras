3f60a350a4a691eb7d6f3847b3e2c454acfbacaa
Rodry03 <03.rodry.03@gmail.com>
2018-01-31 09:21:43 +0100

    cambios vista 2

--- LluviaDeLetras/src/lluviadeletras/Vista.java
+++ LluviaDeLetras/src/lluviadeletras/Vista.java
@@ -26,8 +26,7 @@ public class Vista extends JFrame {
     private JPanel barra, bloque;
 
     private int y = 0;
-    private int x=0;
-
+    private int x = 0;
 
     public Vista(Controlador c) {
         this.c = c;
@@ -36,7 +35,7 @@ public class Vista extends JFrame {
         this.setResizable(false);
         y = -20;
         letras = new ArrayList();
-                crearBloque();
+        crearBloque();
 
         crearBarra();
 
@@ -45,14 +44,13 @@ public class Vista extends JFrame {
 //    a.setBounds(60,60,200,200);
 //    letras.add(a);
 //    this.add(a);
- this.addKeyListener(c);
-    this.setResizable(false);
-    this.setBounds(50,50,600,600);
-    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    this.setVisible(true);
-    
-    
-}
+        this.addKeyListener(c);
+        this.setResizable(false);
+        this.setBounds(50, 50, 600, 600);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+    }
 
     public void crearLetras(String letra) {
         System.out.println(letra);
@@ -62,40 +60,42 @@ public class Vista extends JFrame {
         lb.setBounds(x, 10, 25, 25);
         this.add(lb);
         letras.add(lb);
-       
-        
-                
+
     }
-    public void cambiarY(){
-       
+
+    public void cambiarY() {
+
         for (int i = 0; i < letras.size(); i++) {
-            letras.get(i).setBounds(letras.get(i).getX(),letras.get(i).getY()+10, 25,25);
+            letras.get(i).setBounds(letras.get(i).getX(), letras.get(i).getY() + 10, 25, 25);
         }
-       this.repaint();
+        this.repaint();
     }
-
-
 
     public void crearBarra() {
-        barra=new JPanel();
+        barra = new JPanel();
         barra.setBounds(0, 500, 600, 20);
         barra.setBackground(Color.red);
         this.add(barra);
     }
-    
-    public void crearBloque(){
-        bloque=new JPanel();
+
+    public void crearBloque() {
+        bloque = new JPanel();
         bloque.setBounds(275, 500, 50, 20);
         bloque.setBackground(Color.black);
         this.add(bloque);
     }
-    
-    public void moverBloqueDerecha(){
-        bloque.setBounds(bloque.getX()+10, bloque.getY(), 50, 20);
+
+    public void moverBloqueDerecha() {
+        if(bloque.getX()<=550){
+            bloque.setBounds(bloque.getX() + 10, bloque.getY(), 50, 20);
+        }
+        
     }
-    
-    public void moverBloqueIzquierda(){
-                bloque.setBounds(bloque.getX()-10, bloque.getY(), 50, 20);
+ 
+    public void moverBloqueIzquierda() {
+        if(bloque.getX()>=0){
+            bloque.setBounds(bloque.getX() - 10, bloque.getY(), 50, 20);
+        }
 
     }
 
